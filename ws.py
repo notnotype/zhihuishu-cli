965d183fdc62a63328b2afa241bd5d18929d5167
notnotype <=>
2021-04-30 23:20:11 +0800

    feat ws.py finish login function

--- ws.py
+++ ws.py
@@ -6,7 +6,7 @@ from base64 import b64decode, b64encode
 from datetime import datetime, timedelta
 
 import requests
-from requests import get, post, RequestException
+from requests import post, RequestException
 from requests.cookies import RequestsCookieJar, merge_cookies
 from websocket import create_connection
 from loguru import logger
@@ -26,51 +26,6 @@ except ImportError:
 
 
     logger.info('没有安装pillow')
-
-
-class ZhiHuiShu:
-    headers: Dict
-    client: requests.Session
-
-    def __init__(self):
-        self.client = requests.Session()
-
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.3'
-                  '6 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-    'sec-ch-ua-mobile': '?0',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1'
-}
-
-# cookies: Optional[RequestsCookieJar] = RequestsCookieJar()
-
-client = requests.Session()
-
-
-@wraps(requests.get)
-def get(*args, **kwargs):
-    # kwargs.setdefault('cookies', cookies)
-    kwargs.setdefault('headers', headers)
-    return client.get(*args, **kwargs)
-
-
-def get_qr() -> str:
-    # get qrcode
-    qrcode_url = 'https://passport.zhihuishu.com/qrCodeLogin/getLoginQrImg'
-    response = get(qrcode_url)
-    response.raise_for_status()
-    json_data = loads(response.text)
-
-    with open('qrcode.jpg', 'wb') as f:
-        f.write(b64decode(json_data['img']))
-    return json_data['qrToken']
 
 
 class DateTimeTimer:
@@ -94,66 +49,93 @@ class DateTimeTimer:
         return bool(self.start_time + self.delta > datetime.now())
 
 
-class DateTimeCounter:
-    ...
+class ZhiHuiShu:
+    headers: Dict
+    client: requests.Session
+
+    def __init__(self):
+        self.client = requests.Session()
+
+    @wraps(requests.get)
+    def get(self, *args, **kwargs):
+        # kwargs.setdefault('cookies', cookies)
+        kwargs['headers'] = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.3'
+                          '6 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+            'sec-ch-ua-mobile': '?0',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1'
+        }.update(kwargs['headers'] if 'headers' in kwargs else {})
+        return self.client.get(*args, **kwargs)
+
+    def get_qr(self) -> str:
+        # get qrcode
+        qrcode_url = 'https://passport.zhihuishu.com/qrCodeLogin/getLoginQrImg'
+        response = self.get(qrcode_url)
+        response.raise_for_status()
+        json_data = loads(response.text)
+
+        with open('qrcode.jpg', 'wb') as f:
+            f.write(b64decode(json_data['img']))
+        return json_data['qrToken']
+
+    @staticmethod
+    def wait_scan(qr_token: str) -> Tuple[str, str]:
+        ws_url = f'wss://appcomm-user.zhihuishu.com/app-commserv-user/websocket?qrToken={qr_token}'
+        ws = create_connection(ws_url)
+
+        timer = DateTimeTimer(90, sep_time=2)  # 30 seconds
+        response = None
+        while timer:
+            ws.send(qr_token)
+            response = ws.recv()
+            json_data = loads(response)
+            if 'code' not in json_data or json_data['code'] != 1:
+                logger.info('检测是否扫码, msg: {}', json_data['msg'])
+                continue
+            password = json_data['oncePassword']
+            uuid = json_data['uuid']
+            break
+        else:
+            # noinspection PyUnboundLocalVariable
+            raise RuntimeError('扫码超时', response)
+        return password, uuid
+
+    def login_pwd(self, pwd: str, uuid: str):
+        url = f'https://passport.zhihuishu.com/login?pwd={pwd}&' \
+              'service=https://onlineservice.zhihuishu.com/login/gologin'
+        response = self.get(url)
+        response.raise_for_status()
+        if len(response.history) < 2:
+            raise RuntimeError('登录失败, 未知错误')
+
+    def login(self):
+        # get qr_code
+        qr_token = self.get_qr()
+        logger.info(f'qr_token 为 [{qr_token}]')
+
+        show_qr()
+
+        password, uuid = self.wait_scan(qr_token)
+        logger.info('临时密码 [{}], uuid: [{}]', password, uuid)
+        self.login_pwd(password, uuid)
+
+    def get_user_info(self):
+        url = 'https://onlineservice.zhihuishu.com/login/getLoginUserInfo'
+        response = self.get(url)
+
+        ic(response.cookies, response.headers)
+
+        response.raise_for_status()
+        json_data = dumps(response.json(), ensure_ascii=False)
+        ic(json_data)
 
 
-def wait_scan(qr_token: str) -> Tuple[str, str]:
-    ws_url = url = f'wss://appcomm-user.zhihuishu.com/app-commserv-user/websocket?qrToken={qr_token}'
-    ws = create_connection(url)
-
-    timer = DateTimeTimer(90, sep_time=2)  # 30 seconds
-    response = None
-    while timer:
-        ws.send(qr_token)
-        response = ws.recv()
-        json_data = loads(response)
-        if 'code' not in json_data or json_data['code'] != 1:
-            logger.info('检测是否扫码, msg: {}', json_data['msg'])
-            continue
-
-        password = json_data['oncePassword']
-        uuid = json_data['uuid']
-
-        break
-    else:
-        # noinspection PyUnboundLocalVariable
-        raise RuntimeError('扫码超时', response)
-    return password, uuid
-
-
-def login_pwd(pwd: str, uuid: str):
-    url = f'https://passport.zhihuishu.com/login?pwd={pwd}&' \
-          'service=https://onlineservice.zhihuishu.com/login/gologin'
-    response = get(url)
-    response.raise_for_status()
-
-    if len(response.history) < 2:
-        raise RuntimeError('登录失败, 未知错误')
-
-
-def login():
-    # get qr_code
-    qr_token = get_qr()
-    logger.info(f'qr_token 为 [{qr_token}]')
-
-    show_qr()
-
-    password, uuid = wait_scan(qr_token)
-    logger.info('临时密码 [{}], uuid: [{}]', password, uuid)
-    login_pwd(password, uuid)
-
-
-def get_user_info():
-    url = 'https://onlineservice.zhihuishu.com/login/getLoginUserInfo'
-    response = get(url)
-
-    ic(response.cookies, response.headers)
-
-    response.raise_for_status()
-    json_data = dumps(response.json(), ensure_ascii=False)
-    ic(json_data)
-
-
-login()
-get_user_info()
+zhs = ZhiHuiShu()
+zhs.login()
+zhs.get_user_info()
